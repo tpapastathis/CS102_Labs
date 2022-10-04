74a72c2b0b65e72242f565551b4499493cce17d1
Ted Papastathis <tpapastathis1234@gmail.com>
2022-10-04 16:01:18 -0500

    selection sort

--- lab04/lab04b/src/lab04b/MainClass.java
+++ lab04/lab04b/src/lab04b/MainClass.java
@@ -3,8 +3,29 @@ package lab04b;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] myArray1 = {4, 7, 2, 3, 1};
+		
+		mySort(myArray1);
+		
 
+	}
+	public static void mySort(int[] inputArray) {
+		int arraySize = inputArray.length;
+		for (int i = 0; i < arraySize - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < arraySize; j++) {
+				if (inputArray[j] < inputArray[index]){
+					index = j;
+					}
+				}
+			int temp = inputArray[i];
+			inputArray[i] = inputArray[index];
+			inputArray[index] = temp;
+		}
+		
+		for(int i = 0; i < inputArray.length; i++) {
+			System.out.print(inputArray[i]+" ");
+		}
 	}
 
 }
