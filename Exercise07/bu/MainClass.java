03e2f8130b6ba2c19f6b5d2f528a8bbf1d2d0766
Ted Papastathis <tpapastathis1234@gmail.com>
2022-10-20 14:32:21 -0500

    Exercise 07

---
+++ Exercise07/bu/MainClass.java
@@ -0,0 +1,16 @@
+package bu;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		Rectangle myRectangle = new Rectangle(5,2);
+//		myRectangle.setHeight(20);
+//		myRectangle.draw();
+		
+		
+		Square mySquare = new Square(5);
+		mySquare.draw();
+
+	}
+
+}
---
+++ Exercise07/bu/Square.java
@@ -0,0 +1,14 @@
+package bu;
+
+public class Square extends Rectangle {
+	
+	public Square(int width, int height) {
+		super(width, height);
+	}
+	
+	public Square(int width) {
+		super(width, width);
+	}
+	
+
+}
