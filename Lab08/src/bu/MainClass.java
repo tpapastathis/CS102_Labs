f4a98b3416075f90b0aaccc057a6d392389b5b45
Ted Papastathis <tpapastathis1234@gmail.com>
2022-10-25 21:47:58 -0500

    Lab 08 - No filled boolean

---
+++ Lab08/src/bu/MainClass.java
@@ -0,0 +1,11 @@
+package bu;
+
+public class MainClass {
+	public static void main(String args[]) {
+		Rectangle r1 = new Rectangle(5, 8);
+		r1.draw();
+		Rectangle r2 = new Rectangle(8, 5);
+		r2.draw();
+	}
+
+}
---
+++ Lab08/src/bu/Rectangle.java
@@ -0,0 +1,20 @@
+package bu;
+
+public class Rectangle {
+	int width;
+	int height;
+	
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void draw() {
+		for(int i=0; i<height; i++) {
+			for (int j=0;j<width;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
